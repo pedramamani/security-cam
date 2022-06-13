49ea301c2bb185c40e030966051233b820b1701c
Pedram Amani <amani.pedram@gmail.com>
2022-06-12 23:17:48 -0700

    change settings

--- constants.py
+++ constants.py
@@ -2,10 +2,10 @@ CAPTURE_SOURCE = 0
 
 
 # Windows settings: 640x480@10/15/30, 1280x720@10/15/30, 1920x1080@10/15/30
-# Raspbian settings: 1920x1088@30, 720@60, 480p@60/90
+# Raspbian settings: 1920x1088@30, 1280x720@60, 480p@60/90
 
 class SETTING:
-    resolution = (1280, 720)
+    resolution = (640, 480)
     frameRate = 60
 
 class OS:
